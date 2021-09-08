8e9e1df2093a3ca06441da4d510c26bc83541ed5
sevelantis <oskroba.miron@gmail.com>
2021-09-08 18:38:33 +0200

    success start system on hardware boot

--- start.sh
+++ start.sh
@@ -1,19 +1,29 @@
 #!/bin/bash -eux
 
+function get_pod_ip()
+{
+    pod_name=$1
+    echo $(/usr/bin/docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${pod_name})
+}
+
 # down
 /usr/local/bin/docker-compose -f /home/pi/weather-station/docker-compose.yml down
 
 # up
 /usr/local/bin/docker-compose -f /home/pi/weather-station/docker-compose.yml up -d
 
-sleep 3
+# get influxdb local IP
+host=$(get_pod_ip influxdb)
+while -z "${host}"; do
+    host=$(get_pod_ip influxdb)
+    sleep 2
+done
 
-# check influxdb connection (otherwise app fails connecting because its too fast)
-host=$(docker exec -it influxdb hostname -I | awk '{print$1}')
+# wait  for influxdb until it opens a port
 port="8086"
-while ! nc -z ${host} ${port}; do   
+while ! nc -z "${host}" "${port}"; do   
     echo 'waiting for influxdb..'
-    sleep 1
+    sleep 2
 done
 echo 'INFLUXDB READY'
 
