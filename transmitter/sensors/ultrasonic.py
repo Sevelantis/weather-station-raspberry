ed226f80a07eadeb0b0159d11289e6a5429f7298
sevelantis <oskroba.miron@gmail.com>
2021-09-26 14:19:10 +0200

    Added hcsr04 library module

--- transmitter/sensors/ultrasonic.py
+++ transmitter/sensors/ultrasonic.py
@@ -12,8 +12,8 @@ Inserted two pull-up resistors 470ohm and 470ohm, binding together ECHO and GND
 This is because HR-SR04 works at 5V and GPIO pins works at max 3.3V
 ECHO->470->GPIO->470->GND
 '''
-import time
-import RPi.GPIO as GPIO
+import board
+from adafruit_hcsr04 import HCSR04
 from transmitter.sensors.sensor import Sensor
 import logging
 
@@ -26,21 +26,9 @@ class Ultrasonic(Sensor):
         self.sensor_id = self.name
         self.location = 'Wrocław'
 
-        # Init Device
-        #GPIO Mode (BOARD / BCM)
-        GPIO.setmode(GPIO.BCM)
-
-        #set GPIO Pins
-        self.GPIO_ECHO = 24
-        self.GPIO_TRIGGER = 14
-
-        #set GPIO direction (IN / OUT)
-        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
-        GPIO.setup(self.GPIO_ECHO, GPIO.IN)
-
     def get_sensor_data(self):
         try:
-            distance = self.get_distance()
+            distance = self.dev.distance
             if distance:
                 return [
                     ('distance', distance)
@@ -54,29 +42,10 @@ class Ultrasonic(Sensor):
             self.dev.exit()
             raise error
 
-    def get_distance(self):
-        # set Trigger to HIGH
-        GPIO.output(self.GPIO_TRIGGER, True)
-    
-        # set Trigger after 0.01ms to LOW
-        time.sleep(0.00001)
-        GPIO.output(self.GPIO_TRIGGER, False)
-    
-        StartTime = time.time()
-        StopTime = time.time()
-    
-        # save StartTime
-        while GPIO.input(self.GPIO_ECHO) == 0:
-            StartTime = time.time()
-    
-        # save time of arrival
-        while GPIO.input(self.GPIO_ECHO) == 1:
-            StopTime = time.time()
-    
-        # time difference between start and arrival
-        TimeElapsed = StopTime - StartTime
-        # multiply with the sonic speed (34300 cm/s)
-        # and divide by 2, because there and back
-        distance = (TimeElapsed * 34300) / 2
-    
-        return distance
+    def run(self) -> None:
+        # Init Device
+        try:
+            self.dev = HCSR04(trigger_pin=board.D14, echo_pin=board.D24)
+        except Exception as e:
+            logging.info(f'{self.name}: Init failed, reason: {e}')
+        super().run()
