19dea2138b21720494e7891b5adbbe6f0b096a62
sevelantis <oskroba.miron@gmail.com>
2021-10-05 23:36:37 +0200

    added is_influxdb_visible function -check return code of netcat command

--- start.sh
+++ start.sh
@@ -12,6 +12,19 @@ function get_pod_ip()
     echo $(/usr/bin/docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${pod_name})
 }
 
+function is_influxdb_visible()
+{
+    host=$1
+    port=$2
+    nc -z "${host}" "${port}"
+    ret=$?
+    if [ $ret -eq 0 ]; then
+        echo 0
+    else
+        echo 1
+    fi
+}
+
 IGNORE_DOCKER=false
 while getopts "hr" OPT; do
     case ${OPT} in
@@ -28,25 +41,29 @@ done
 
 if [[ ${IGNORE_DOCKER} = false ]]; then
     # down
-    /usr/local/bin/docker-compose -f /home/pi/weather-station/docker-compose.yml down
+    # /usr/local/bin/docker-compose -f /home/pi/weather-station/docker-compose.yml down
 
     # up
-    /usr/local/bin/docker-compose -f /home/pi/weather-station/docker-compose.yml up -d
+    # /usr/local/bin/docker-compose -f /home/pi/weather-station/docker-compose.yml up -d
 
     # get influxdb local IP
     host=$(get_pod_ip influxdb)
-    while -z "${host}"; do
+    echo "host = $host"
+    while [ ! -n "${host}" ]; do
         host=$(get_pod_ip influxdb)
+        echo "Waiting for pod ip.. host=$host"
         sleep 2
     done
 
     # wait  for influxdb until it opens a port 
     port="8086"
-    while ! nc -z "${host}" "${port}"; do   
-        echo 'waiting for influxdb..'
+
+    while [ $(is_influxdb_visible $host $port) -ne 0 ]; do
+        echo "host=$host, port=$port"
+        echo "waiting for influxdb.."
         sleep 2
     done
-    echo 'INFLUXDB READY'
+    echo $(nc -vz "${host}" "${port}")
 fi
 
 # process pulsein is not letting PIN23 to free, kill it
@@ -61,6 +78,6 @@ PID=$(ps auxf | grep pulseio | grep gpiochip0 | grep 24 | awk '{print$2}')
 PID=$(ps auxf | grep entrypoint.py | grep python | awk '{print$2}')
 [ ! -z $PID ] && kill $PID
 
-/usr/bin/python3 /home/pi/weather-station/entrypoint.py > /home/pi/weather-station/aa 2>&1 &
+# /usr/bin/python3 /home/pi/weather-station/entrypoint.py > /home/pi/weather-station/aa 2>&1 &
 
 ps auxf | grep entrypoint.py | grep python
